6f4b037400480ef0c830fde80b69210b094744d1
Aditya <kohsa9211420@gmail.com>
2023-08-23 18:34:54 +0530

    added codeExecution func

--- server.js
+++ server.js
@@ -1,66 +1,134 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const ACTIONS = require('./src/Actions');
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
+const ACTIONS = require("./src/Actions");
+const cors = require("cors");
+var compiler = require("compilex");
+var options = { stats: true }; //prints stats on console
+compiler.init(options);
 
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("build"));
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
-    // Map
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    );
+  // Map
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
 }
 
-io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+io.on("connection", (socket) => {
+  console.log("socket connected", socket.id);
 
-    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-        userSocketMap[socket.id] = username;
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit(ACTIONS.JOINED, {
-                clients,
-                username,
-                socketId: socket.id,
-            });
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
+
+  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
+
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
+});
+
+app.post("/compile", function (req, res) {
+  try {
+    var code = req.body.code;
+    var input = req.body.input;
+    var lang = req.body.lang;
+
+    if (lang == "python") {
+      if (input.length > 0) {
+        var envData = { OS: "windows" };
+
+        compiler.compilePythonWithInput(envData, code, input, function (data) {
+          return res.send(data);
         });
-    });
+      } else {
+        var envData = { OS: "windows" };
 
-    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
-
-    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
-
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomId) => {
-            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-            });
+        compiler.compilePython(envData, code, function (data) {
+          return res.send(data);
         });
-        delete userSocketMap[socket.id];
-        socket.leave();
-    });
+      }
+    } else if (lang == "cpp") {
+      if (input.length > 0) {
+        var envData = { OS: "windows", cmd: "g++" }; // (uses g++ command to compile )
+        //else
+        compiler.compileCPPWithInput(envData, code, input, function (data) {
+          return res.send(data);
+        });
+      } else {
+        var envData = { OS: "windows", cmd: "g++" }; // (uses g++ command to compile )
+        //else
+        compiler.compileCPP(envData, code, function (data) {
+          return res.send(data);
+          //data.error = error message
+          //data.output = output value
+        });
+      }
+    } else if (lang == "java") {
+      if (input.length > 0) {
+        var envData = { OS: "windows" };
+        //else
+        compiler.compileJavaWithInput(envData, code, input, function (data) {
+          return res.send(data);
+        });
+      } else {
+        //if windows
+        var envData = { OS: "windows" };
+        //else
+        compiler.compileJava(envData, code, function (data) {
+          return res.send(data);
+        });
+      }
+    }
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  //data.error = error message
+  //data.output = output value
 });
 
 const PORT = process.env.PORT || 5000;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,57 +1,129 @@
-import React, { useEffect, useRef } from 'react';
-import Codemirror from 'codemirror';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/dracula.css';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/closebrackets';
-import ACTIONS from '../Actions';
+import React, { useEffect, useRef, useState } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import ACTIONS from "../Actions";
+import axios from "axios";
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
-    const editorRef = useRef(null);
-    useEffect(() => {
-        async function init() {
-            editorRef.current = Codemirror.fromTextArea(
-                document.getElementById('realtimeEditor'),
-                {
-                    mode: { name: 'javascript', json: true },
-                    theme: 'dracula',
-                    autoCloseTags: true,
-                    autoCloseBrackets: true,
-                    lineNumbers: true,
-                }
-            );
+  const [input, setInput] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("cpp");
+  const [code, setCode] = useState("");
+  const [result, setresult] = useState("");
 
-            editorRef.current.on('change', (instance, changes) => {
-                const { origin } = changes;
-                const code = instance.getValue();
-                onCodeChange(code);
-                if (origin !== 'setValue') {
-                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
-                        roomId,
-                        code,
-                    });
-                }
-            });
+  const editorRef = useRef(null);
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+    editorRef.current.setOption("mode", event.target.value);
+  };
+
+  const handleInputChange = (newInput) => {
+    setInput(newInput);
+  };
+
+  const handleSubmit = async (e) => {
+    const stringWithoutNewlines = code.replace(/\n/g, " ");
+    const body = {
+      code: stringWithoutNewlines,
+      lang: selectedLanguage,
+      input: input,
+    };
+
+    const { data } = await axios.post("http://localhost:5000/compile", body);
+
+    setresult(data);
+  };
+
+  useEffect(() => {
+    async function init() {
+      editorRef.current = Codemirror.fromTextArea(
+        document.getElementById("realtimeEditor"),
+        {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
         }
-        init();
-    }, []);
+      );
 
-    useEffect(() => {
-        if (socketRef.current) {
-            socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
-                if (code !== null) {
-                    editorRef.current.setValue(code);
-                }
-            });
+      editorRef.current.on("change", (instance, changes) => {
+        const { origin } = changes;
+        const code = instance.getValue();
+        onCodeChange(code);
+        setCode(code);
+        if (origin !== "setValue") {
+          socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+            roomId,
+            code,
+          });
         }
+      });
+    }
+    init();
+  }, []);
 
-        return () => {
-            socketRef.current.off(ACTIONS.CODE_CHANGE);
-        };
-    }, [socketRef.current]);
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+          console.log(code);
+        }
+      });
+    }
 
-    return <textarea id="realtimeEditor"></textarea>;
+    return () => {
+      socketRef.current.off(ACTIONS.CODE_CHANGE);
+    };
+  }, [socketRef.current]);
+
+  return (
+    <div className="editor-container">
+      <div className="editor">
+        <textarea
+          id="realtimeEditor"
+          // Update code state on change
+        ></textarea>
+      </div>
+      <div className="bottom">
+        <div className="input-section">
+          <p>Input</p>
+          <textarea
+            className="input-box"
+            value={input}
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
+        </div>
+        <div className="output-section">
+          <p>Output</p>
+          <div className="output-box">
+            {result.output
+              ? JSON.stringify(result.output) // Convert to string if it's an object
+              : result.error
+              ? result.error
+              : ""}
+          </div>
+        </div>
+
+        <div className="language-select">
+          <select value={selectedLanguage} onChange={handleLanguageChange}>
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+            <option value="cpp">C++</option>
+            {/* Add more options as needed */}
+          </select>
+        </div>
+        <button className="btn runBtn" onClick={handleSubmit}>
+          Run Code
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
